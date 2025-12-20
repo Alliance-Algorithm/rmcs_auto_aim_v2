96a5a973981581bc393d7ab7c0a82dd52a6dcedd
heyeuu <2829004293@qq.com>
2025-12-20 22:07:53 +0800

    feat(predictor): implement RobotState encapsulation for car with Kalman Filter

--- src/utility/math/conversion.hpp
+++ src/utility/math/conversion.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include "utility/math/angle.hpp"
 #include <eigen3/Eigen/Geometry>
+#include <opencv2/core/cvdef.h>
 
 namespace rmcs::util {
 
@@ -11,18 +12,20 @@ inline const Eigen::Matrix3d kCoordTransformMatrix {
     { +0, -1, +0 },
 };
 
-inline auto opencv2ros_position(const Eigen::Vector3d& position) -> Eigen::Vector3d {
+inline constexpr auto opencv2ros_position(const Eigen::Vector3d& position) -> Eigen::Vector3d {
     auto result = Eigen::Vector3d { position.z(), -position.x(), -position.y() };
     return result;
 }
-inline auto opencv2ros_rotation(const Eigen::Matrix3d& rotation_matrix) -> Eigen::Matrix3d {
+inline constexpr auto opencv2ros_rotation(const Eigen::Matrix3d& rotation_matrix)
+    -> Eigen::Matrix3d {
     return kCoordTransformMatrix * rotation_matrix * kCoordTransformMatrix.transpose();
 }
-inline auto ros2opencv_position(const Eigen::Vector3d& position) -> Eigen::Vector3d {
+inline constexpr auto ros2opencv_position(const Eigen::Vector3d& position) -> Eigen::Vector3d {
     auto result = Eigen::Vector3d { -position.y(), -position.z(), position.x() };
     return result;
 }
-inline auto ros2opencv_rotation(const Eigen::Matrix3d& rotation_matrix) -> Eigen::Matrix3d {
+inline constexpr auto ros2opencv_rotation(const Eigen::Matrix3d& rotation_matrix)
+    -> Eigen::Matrix3d {
     return kCoordTransformMatrix.transpose() * rotation_matrix * kCoordTransformMatrix;
 }
 
