5745b275c33b215ca104d4259099b2bb86f13bb8
heyeuu <2829004293@qq.com>
2026-01-05 22:14:35 +0800

    refactor(fire_control): optimize configuration initialization logic

--- src/kernel/fire_control.hpp
+++ src/kernel/fire_control.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-<<<<<<< HEAD
 #include <expected>
 #include <yaml-cpp/yaml.h>
 
@@ -8,19 +7,10 @@
 #include "utility/clock.hpp"
 #include "utility/math/linear.hpp"
 #include "utility/pimpl.hpp"
-=======
-#include <eigen3/Eigen/Geometry>
-#include <expected>
-#include <yaml-cpp/yaml.h>
-
-#include "utility/pimpl.hpp"
-#include "utility/robot/armor.hpp"
->>>>>>> c2da77f (feat(logic): implement target selection logic and decouple from visualization)
 
 namespace rmcs::kernel {
 
 class FireControl {
-<<<<<<< HEAD
     using Clock = util::Clock;
 
     RMCS_PIMPL_DEFINITION(FireControl)
@@ -38,14 +28,5 @@ public:
 
     auto solve(const predictor::Snapshot& snapshot, Translation const& odom_to_muzzle_translation)
         -> std::optional<Result>;
-=======
-    RMCS_PIMPL_DEFINITION(FireControl)
-
-public:
-    auto initialize(const YAML::Node&) noexcept -> std::expected<void, std::string>;
-
-    auto choose_armor(std::span<const Armor3D> armors, const Eigen::Vector<double, 11>& ekf_x)
-        -> std::optional<Armor3D>;
->>>>>>> c2da77f (feat(logic): implement target selection logic and decouple from visualization)
 };
 }
