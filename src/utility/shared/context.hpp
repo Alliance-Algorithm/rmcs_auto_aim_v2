0da1b272c0af49046fc73479cfa2104bcbde004c
creeper5820 <cqq1960180796@163.com>
2026-05-01 18:19:09 +0800

    refactor: rename ControlState and transform fields

--- src/utility/shared/context.hpp
+++ src/utility/shared/context.hpp
@@ -3,8 +3,6 @@
 #include "utility/clock.hpp"
 #include "utility/math/linear.hpp"
 #include "utility/robot/id.hpp"
-#include <cmath>
-#include <limits>
 
 namespace rmcs {
 
@@ -17,33 +15,6 @@ enum class ShootMode {
     BATTLE,
     BUFF_SMALL,
     BUFF_LARGE,
-};
-
-struct Transform {
-    Translation position { };
-    Orientation orientation { };
-
-    static constexpr auto kNaN() {
-        return Transform {
-            Translation {
-                std::numeric_limits<double>::quiet_NaN(),
-                std::numeric_limits<double>::quiet_NaN(),
-                std::numeric_limits<double>::quiet_NaN(),
-            },
-            Orientation {
-                std::numeric_limits<double>::quiet_NaN(),
-                std::numeric_limits<double>::quiet_NaN(),
-                std::numeric_limits<double>::quiet_NaN(),
-                std::numeric_limits<double>::quiet_NaN(),
-            },
-        };
-    };
-    static constexpr auto kIdentity() {
-        return Transform {
-            Translation { 0, 0, 0 },
-            Orientation { 0, 0, 0, 1 },
-        };
-    }
 };
 
 struct AutoAimState {
@@ -73,7 +44,7 @@ struct AutoAimState {
 };
 static_assert(context_trait<AutoAimState>);
 
-struct ControlState {
+struct SystemContext {
     static constexpr auto kLabel  = "/shm_control_state";
     static constexpr auto kLength = 512;
 
@@ -87,38 +58,31 @@ struct ControlState {
 
     Transform camera_transform { }; // Imu Odom Link
 
-    struct {
-        TimePoint timestamp = Clock::now();
-        std::uint64_t index = 0;
-    } capture_signals;
-
     /// Lazy Context
     ///
     DeviceIds invincible_devices { DeviceIds::None() };
 
     static auto kInvalid() {
-        return ControlState {
+        return SystemContext {
             .timestamp          = Clock::now(),
             .shoot_mode         = ShootMode::STOPPING,
             .yaw                = std::numeric_limits<double>::quiet_NaN(),
             .pitch              = std::numeric_limits<double>::quiet_NaN(),
             .camera_transform   = Transform::kNaN(),
-            .capture_signals    = { },
             .invincible_devices = DeviceIds::None(),
         };
     }
     static auto kIdentity() {
-        return ControlState {
+        return SystemContext {
             .timestamp          = Clock::now(),
             .shoot_mode         = ShootMode::BATTLE,
             .yaw                = 0,
             .pitch              = 0,
             .camera_transform   = Transform::kIdentity(),
-            .capture_signals    = { },
             .invincible_devices = DeviceIds::None(),
         };
     }
 };
-static_assert(context_trait<ControlState>);
+static_assert(context_trait<SystemContext>);
 
 } // namespace rmcs
